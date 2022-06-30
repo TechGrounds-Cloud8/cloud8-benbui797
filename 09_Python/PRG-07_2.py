ee494f0fd72ad445337a48b39d00066ee993a228
Ben Buitendijk <benbuitendijk797@gmail.com>
2022-06-30 09:34:32 +0200

    PRG-07 update comment

--- 09_Python/PRG-07_2.py
+++ 09_Python/PRG-07_2.py
@@ -3,8 +3,8 @@ list = [9, 80, 16, 67, 35]
 print(f"My list: {list}")
 
 # enumerate adds a number (the index) to every element in the list. 
-# you have to assign a variable to both elements (in this case i and j)
-# it is useful in this situation, but range gives more flexibility (you can define start, stop and step)
+# you have to assign a variable to both elements in your for-loop (in this case named; i and j)
+# it is useful in this situation, but range can give more flexibility (you can control start, stop and step)
 
 for i, j in enumerate(list):
     # print(f"{i=} {j=}")                   # uncomment this to see the values of i and j printed out!
